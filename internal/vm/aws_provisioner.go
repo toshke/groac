c1b62b0f1fd2e939aef88a0258752d7713156ab9
Nikola Tosic <tosicnikola10@gmail.com>
2021-06-09 20:28:55 +1000

    wip: implemented subnet discovery, and some refactor

--- internal/vm/aws_provisioner.go
+++ internal/vm/aws_provisioner.go
@@ -3,6 +3,7 @@ package vm
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -10,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/rs/zerolog/log"
 )
 
@@ -35,7 +37,7 @@ func NewVmProvisionerAws() *VmProvisionerAws {
 	return &provisioner
 }
 
-func (provisioner *VmProvisionerAws) Provision(groacConfig map[string]string) *AwsVm {
+func (provisioner *VmProvisionerAws) Provision(groacConfig map[string]string, sequence int) *AwsVm {
 	params := &ec2.RunInstancesInput{}
 
 	params.MaxCount = aws.Int32(1)
@@ -48,15 +50,53 @@ func (provisioner *VmProvisionerAws) Provision(groacConfig map[string]string) *A
 	//TODO: parametrise keypair name, allow configuration - provided value
 
 	// create key if does not exist
-	readKeyPairParams := &ec2.DescribeKeyPairsInput{KeyNames: []string{"groac-gitlab-runner-key"}}
-	readKeyOutput, err := provisioner.ec2Client.DescribeKeyPairs(context.TODO(), readKeyPairParams)
-	publicKeyMaterial, err := publicKeyMaterial(connectionParams.publicKeyPath)
+	keyPairName, err := discoverKeyPair(provisioner.ec2Client, groacConfig, connectionParams.publicKeyPath)
+	check(err)
+	params.KeyName = keyPairName
+
+	// subnet discovery
+	subnetId, err := discoverSubnetId(provisioner.ec2Client, groacConfig, sequence)
+	check(err)
+	params.SubnetId = subnetId
+
+	// security group discovery
+	sgIds, err := discoverSecurityGroupIds(provisioner.ec2Client, groacConfig)
+	check(err)
+	params.SecurityGroupIds = sgIds
+	//TODO: create Security Group if none given, allowing for SSH and Docker connection
+
+	//TODO: configure context
+
+	runInstanceOut, err := provisioner.ec2Client.RunInstances(context.TODO(), params)
+	check(err)
+
+	var awsVm AwsVm
+	awsVm.connectionParams.hostname = *runInstanceOut.Instances[0].PrivateIpAddress
+
+	return &awsVm
+}
+
+// return keypair used for groac. Discover based on default key pair name, and verify
+// fingerprints. If fingerprint does not match, created new key pair for given name with
+// timestamp suffix
+func discoverKeyPair(client *ec2.Client, groacConfig map[string]string, publicKeyPath string) (*string, error) {
 	var keyPairName *string
 	if val, ok := groacConfig["awsKeyPairName"]; ok {
 		keyPairName = aws.String(val)
 	} else {
 		keyPairName = aws.String("groac-gitlab-runner-key")
 	}
+	readKeyPairParams := &ec2.DescribeKeyPairsInput{KeyNames: []string{*keyPairName}}
+	readKeyOutput, err := client.DescribeKeyPairs(context.TODO(), readKeyPairParams)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKeyMaterial, err := publicKeyMaterial(publicKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
 	// if the key exists with different finger print groac-gitlab-runner-key-timestamp key will be created
 	if err != nil {
 		// check if error claims that key does not exist
@@ -65,40 +105,72 @@ func (provisioner *VmProvisionerAws) Provision(groacConfig map[string]string) *A
 				KeyName:           keyPairName,
 				PublicKeyMaterial: []byte(base64.StdEncoding.EncodeToString(publicKeyMaterial)),
 			}
-			importResponse, err := provisioner.ec2Client.ImportKeyPair(context.TODO(), importKeyPairParams)
-			check(err)
+			importResponse, err := client.ImportKeyPair(context.TODO(), importKeyPairParams)
+			if err != nil {
+				return nil, err
+			}
 			log.Info().Str("ProvisionedGroacKeyId", *importResponse.KeyPairId)
 			log.Info().Str("ProvisionedGroacKeyName", *keyPairName)
 		} else {
-			check(err)
+			return nil, err
 		}
 	} else {
 		// verify that keypair fingerprent matches local one
 		awsFingerprint := *readKeyOutput.KeyPairs[0].KeyFingerprint
 		localFingerprint := publicKeyFingerPrint(publicKeyMaterial)
 		if awsFingerprint != localFingerprint {
-			keyPairName = aws.String("groac-gitlab-runner-key-" + fmt.Sprintf("%d", time.Now().Unix()))
+			keyPairName = aws.String(*keyPairName + "-" + fmt.Sprintf("%d", time.Now().Unix()))
 			importKeyPairParams := &ec2.ImportKeyPairInput{
 				KeyName:           keyPairName,
 				PublicKeyMaterial: []byte(base64.StdEncoding.EncodeToString(publicKeyMaterial)),
 			}
-			importResponse, err := provisioner.ec2Client.ImportKeyPair(context.TODO(), importKeyPairParams)
+			importResponse, err := client.ImportKeyPair(context.TODO(), importKeyPairParams)
 			check(err)
 			log.Info().Str("ProvisionedGroacKeyId", *importResponse.KeyPairId)
 			log.Info().Str("ProvisionedGroacKeyName", *keyPairName)
 		}
 	}
+	return keyPairName, nil
+}
 
-	params.KeyName = keyPairName
+// pull out subnet from configuration, all discover default subnet. Azs are being
+// picked by round robin based on sequence of the machine being provisioned
+func discoverSubnetId(client *ec2.Client, groacConfig map[string]string, sequence int) (*string, error) {
+	if val, ok := groacConfig["SubnetIds"]; ok {
+		allValues := strings.Split(val, ",")
+		return &allValues[sequence%len(allValues)], nil
+	} else {
+		// discover subnet
+		// list all zones
+		azs, _ := client.DescribeAvailabilityZones(context.TODO(), &ec2.DescribeAvailabilityZonesInput{})
+		allValues := azs.AvailabilityZones
+		selectAz := allValues[sequence%len(allValues)]
+		log.Info().Str("selectAZ", *selectAz.ZoneName)
+		vpcResp, _ := client.DescribeVpcs(context.TODO(),
+			&ec2.DescribeVpcsInput{Filters: []types.Filter{
+				{Name: aws.String("isDefault"), Values: []string{"true"}},
+			}})
+		if len(vpcResp.Vpcs) < 1 {
+			return nil, errors.New("subnets not suplied via SubnetIds parameter, and no default VPC found ")
+		}
+		vpcId := vpcResp.Vpcs[0].VpcId
+		log.Info().Str("defaultVpcId", *vpcId)
+		subnetResponse, _ := client.DescribeSubnets(context.TODO(), &ec2.DescribeSubnetsInput{
+			Filters: []types.Filter{
+				{Name: aws.String("vpc-id"), Values: []string{*vpcId}},
+				{Name: aws.String("availability-zone-id"), Values: []string{*selectAz.ZoneId}},
+				{Name: aws.String("default-for-az"), Values: []string{"true"}},
+			}})
+		if len(subnetResponse.Subnets) < 1 {
+			return nil, fmt.Errorf(
+				"subnets not suplied via SubnetIds parameter, and no default subnet for AZ %s",
+				*selectAz.ZoneName,
+			)
+		}
+		return subnetResponse.Subnets[0].SubnetId, nil
+	}
+}
 
-	//TODO: create Security Group if none given
-	//TODO: discover default VPC if none given
-	//TODO: pick default subnet if none given
-	output, err := provisioner.ec2Client.RunInstances(context.TODO(), params)
-	check(err)
-
-	var awsVm AwsVm
-	awsVm.connectionParams.hostname = *output.Instances[0].PrivateIpAddress
-
-	return &awsVm
+func discoverSecurityGroupIds(client *ec2.Client, groacConfig map[string]string) ([]string, error) {
+	return nil, nil
 }
