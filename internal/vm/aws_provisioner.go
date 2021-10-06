46544e713febc3d0417ee16a20fffe35f219a9f3
Nikola Tosic <tosicnikola10@gmail.com>
2021-10-06 22:20:12 +0200

    wip: ami and sg discovery for aws provisioner

--- internal/vm/aws_provisioner.go
+++ internal/vm/aws_provisioner.go
@@ -12,6 +12,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
 	"github.com/rs/zerolog/log"
 )
 
@@ -24,6 +26,7 @@ type VmProvisionerAws struct {
 	subnetIds       []string
 	subnetPointer   int
 	ec2Client       *ec2.Client
+	ssmClient       *ssm.Client
 }
 
 func NewVmProvisionerAws() *VmProvisionerAws {
@@ -32,7 +35,9 @@ func NewVmProvisionerAws() *VmProvisionerAws {
 	check(err)
 
 	client := ec2.NewFromConfig(cfg)
+	ssmClient := ssm.NewFromConfig(cfg)
 	provisioner.ec2Client = client
+	provisioner.ssmClient = ssmClient
 
 	return &provisioner
 }
@@ -55,16 +60,18 @@ func (provisioner *VmProvisionerAws) Provision(groacConfig map[string]string, se
 	params.KeyName = keyPairName
 
 	// subnet discovery
-	subnetId, err := discoverSubnetId(provisioner.ec2Client, groacConfig, sequence)
+	subnetId, vpcId, err := discoverSubnetId(provisioner.ec2Client, groacConfig, sequence)
 	check(err)
 	params.SubnetId = subnetId
 
 	// security group discovery
-	sgIds, err := discoverSecurityGroupIds(provisioner.ec2Client, groacConfig)
+	sgIds, err := discoverSecurityGroupIds(provisioner.ec2Client, vpcId, groacConfig)
 	check(err)
 	params.SecurityGroupIds = sgIds
-	//TODO: create Security Group if none given, allowing for SSH and Docker connection
 
+	amiId, err := discoverAmiId(provisioner.ec2Client, groacConfig)
+	check(err)
+	params.ImageId = amiId
 	//TODO: configure context
 
 	runInstanceOut, err := provisioner.ec2Client.RunInstances(context.TODO(), params)
@@ -133,12 +140,21 @@ func discoverKeyPair(client *ec2.Client, groacConfig map[string]string, publicKe
 	return keyPairName, nil
 }
 
-// pull out subnet from configuration, all discover default subnet. Azs are being
+// pull out subnet from configuration, or discover default subnet. Azs are being
 // picked by round robin based on sequence of the machine being provisioned
-func discoverSubnetId(client *ec2.Client, groacConfig map[string]string, sequence int) (*string, error) {
+func discoverSubnetId(client *ec2.Client, groacConfig map[string]string, sequence int) (*string, *string, error) {
 	if val, ok := groacConfig["SubnetIds"]; ok {
 		allValues := strings.Split(val, ",")
-		return &allValues[sequence%len(allValues)], nil
+		subnetId := &allValues[sequence%len(allValues)]
+		subnetResponse, _ := client.DescribeSubnets(context.TODO(),
+			&ec2.DescribeSubnetsInput{
+				SubnetIds: []string{*subnetId},
+			})
+		if len(subnetResponse.Subnets) > 0 {
+			return subnetId, subnetResponse.Subnets[0].VpcId, nil
+		} else {
+			return nil, nil, fmt.Errorf("couldn't discover vpc of subnet %s", *subnetId)
+		}
 	} else {
 		// discover subnet
 		// list all zones
@@ -151,7 +167,7 @@ func discoverSubnetId(client *ec2.Client, groacConfig map[string]string, sequenc
 				{Name: aws.String("isDefault"), Values: []string{"true"}},
 			}})
 		if len(vpcResp.Vpcs) < 1 {
-			return nil, errors.New("subnets not suplied via SubnetIds parameter, and no default VPC found ")
+			return nil, nil, errors.New("subnets not suplied via SubnetIds parameter, and no default VPC found ")
 		}
 		vpcId := vpcResp.Vpcs[0].VpcId
 		log.Info().Str("defaultVpcId", *vpcId)
@@ -162,15 +178,88 @@ func discoverSubnetId(client *ec2.Client, groacConfig map[string]string, sequenc
 				{Name: aws.String("default-for-az"), Values: []string{"true"}},
 			}})
 		if len(subnetResponse.Subnets) < 1 {
-			return nil, fmt.Errorf(
+			return nil, nil, fmt.Errorf(
 				"subnets not suplied via SubnetIds parameter, and no default subnet for AZ %s",
 				*selectAz.ZoneName,
 			)
 		}
-		return subnetResponse.Subnets[0].SubnetId, nil
+		return subnetResponse.Subnets[0].SubnetId, vpcId, nil
 	}
 }
 
-func discoverSecurityGroupIds(client *ec2.Client, groacConfig map[string]string) ([]string, error) {
-	return nil, nil
+// pull out security group ids from configuration or create one
+// if none passed in. Auto-created sg allows outbound access to whole of the internet
+// and inbound access to ssh from specified or discvered vpc
+func discoverSecurityGroupIds(client *ec2.Client, vpcId *string, groacConfig map[string]string) ([]string, error) {
+	if val, ok := groacConfig["SecurityGroupIds"]; ok {
+		allValues := strings.Split(val, ",")
+		return allValues, nil
+	} else {
+		// discver vpc cidr
+		vpcData, _ := client.DescribeVpcs(context.TODO(),
+			&ec2.DescribeVpcsInput{
+				VpcIds: []string{*vpcId},
+			})
+		if len(vpcData.Vpcs) < 1 {
+			return nil, fmt.Errorf("couldn't discover vpc %s", *vpcId)
+		}
+		vpcCidr := vpcData.Vpcs[0].CidrBlock
+		// create security group
+		createSgResponse, _ := client.CreateSecurityGroup(context.TODO(),
+			&ec2.CreateSecurityGroupInput{
+				Description: aws.String("Gitlab Runner auto-created seucurity group"),
+				GroupName:   aws.String("Groac-SecurityGroup"),
+				VpcId:       vpcId,
+			})
+
+		// authorize egress 0.0.0.0
+		client.AuthorizeSecurityGroupEgress(context.TODO(),
+			&ec2.AuthorizeSecurityGroupEgressInput{
+				GroupId: createSgResponse.GroupId,
+				CidrIp:  aws.String("0.0.0.0/0"),
+				IpPermissions: []types.IpPermission{
+					{
+						FromPort:   aws.Int32(0),
+						ToPort:     aws.Int32(65535),
+						IpProtocol: aws.String("-1"),
+						IpRanges: []types.IpRange{
+							{
+								CidrIp: aws.String("0.0.0.0/0"),
+							},
+						},
+					},
+				}})
+
+		// authorize ingree for ssh from vpc
+		// for minimal privlege access users should
+		// pass in predefined secuity groups
+		client.AuthorizeSecurityGroupIngress(context.TODO(),
+			&ec2.AuthorizeSecurityGroupIngressInput{
+				GroupId: createSgResponse.GroupId,
+				CidrIp:  vpcCidr,
+				IpPermissions: []types.IpPermission{
+					{
+						FromPort:   aws.Int32(22),
+						ToPort:     aws.Int32(22),
+						IpProtocol: aws.String("tcp"),
+						IpRanges: []types.IpRange{
+							{
+								CidrIp: vpcCidr,
+							},
+						},
+					},
+				}})
+		return []string{*createSgResponse.GroupId}, nil
+	}
+}
+
+func discoverAmiId(client *ssm.Client, groacConfig map[string]string) (*string, error) {
+	if val, ok := groacConfig["AmiId"]; ok {
+		return &val, nil
+	} else {
+		ssmAmiValue, _ := client.GetParameter(context.TODO(), &ssm.GetParameterInput{
+			Name: aws.String("/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"),
+		})
+		return ssmAmiValue.Parameter.Value, nil
+	}
 }
